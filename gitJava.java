05d62b6e7699aac1f0ef0b2ffd1a9aca26317e00
omiguelgermano <miguelgermano86@gmail.com>
2023-09-14 20:58:11 -0300

    Calculo simples com java, apenas soma com valores fixos.

---
+++ gitJava.java
@@ -0,0 +1,9 @@
+public class gitJava {
+
+    public static void main(String[] args) {
+    int a = 2;
+    int b = 3;
+    System.out.println("A soma é: ");
+    int soma = a + b;
+    System.out.println(soma);
+}}
